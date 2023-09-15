f195aa337f01468faf8575bc8b471bbce97ca4c9
Romas <gircysromualdas@gmail.com>
2023-09-15 15:37:43 +0300

    add tasks

--- login/urls.py
+++ login/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from login.views import (
     indexView,
+    mainView,
 )
 
 urlpatterns = [
     path('', indexView, name='index'),
+    path('main/', mainView, name='main'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
 import json
+import ast
 
 from login.form import LoginForm
 
@@ -11,13 +12,21 @@ def indexView(request):
         if form.is_valid():
             login = loginApi(form.cleaned_data['username'], form.cleaned_data['password'])
             if login['login'] == 'true':
-                return render(request, 'main.html', context = {'login': login,})
+                request.session['login'] = login
+                return redirect('main')
             else:
                 context['error'] = 'Incorrect username or password'
 
     loginForm = LoginForm()
     context['loginForm'] = loginForm
     return render(request, 'index.html', context)
+
+def mainView(request):
+    context = {
+        'tasks': getTaskApi(),
+        'login': request.session['login'],
+    }
+    return render(request, 'main.html', context)
 
 def loginApi(username, password):
     body = {
@@ -29,3 +38,13 @@ def loginApi(username, password):
     response = requests.put(url, data=json.dumps(body), headers=headers)
     data = response.json()
     return data
+
+def getTaskApi():
+    url = 'http://localhost:8080/ords/development/task/getTasks'
+    response = requests.get(url)
+    data = response.text
+    data = data[1:-2]
+    data = data = json.loads(data)
+    return data
+
+
